21952da65742084d11aaa93e642a84ed094bfa35
Amin Saedi <amin.di.sam@gmail.com>
2024-04-20 20:38:48 -0400

    Added basic port checking

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{env, net::TcpStream};
+
+fn scan_port(port: u16) -> bool {
+    match TcpStream::connect(("0.0.0.0", port)) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let port = args[1].parse::<u16>().unwrap();
+    println!("Waiting for port {}", port);
+    while !scan_port(port) {}
+    println!("Found open port! exiting...");
+}
