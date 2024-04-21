9b03bfdbef57c6ee37f739318098ac822dd1fb82
Amin Saedi <amin.di.sam@gmail.com>
2024-04-21 09:33:28 -0400

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ fn scan_port(port: u16) -> bool {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Invalid arguments!\nexample usage: portus 443")
+    }
     let port = args[1].parse::<u16>().unwrap();
     println!("Waiting for port {}", port);
     while !scan_port(port) {}
